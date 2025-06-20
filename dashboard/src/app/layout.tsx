530a1fbca7eff00a6838249a0b8c2a397cb8edb4
Remus Poon <remuspoon@gmail.com>
2025-06-21 00:36:23 +0100

    delete redunant files + add basic layout

--- dashboard/src/app/layout.tsx
+++ dashboard/src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Cabin, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const cabin = Cabin({
+  variable: "--font-cabin",
   subsets: ["latin"],
 });
 
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${cabin.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
