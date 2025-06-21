f77e5ea8998ae9d42793a58205c42a5e6cb51512
Remus Poon <remuspoon@gmail.com>
2025-06-21 11:52:32 +0100

    schema

--- dashboard/src/app/page.tsx
+++ dashboard/src/app/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import AlertCard from './components/alert-card'
 
 export default function Dashboard() {
   return (
@@ -12,12 +13,11 @@ export default function Dashboard() {
         </nav>
         <main className='grid grid-cols-12 px-10 gap-10 bg-light flex-1'>
             <div className='col-start-2 col-span-4 bg-white p-8 rounded-3xl my-5'>
-                <div className='text-4xl font-semibold tracking-wide font-cabin text-dark'>Alerts</div>
-                <div className='flex flex-col gap-4'>
-                  <div>Alert 1</div>
-                  <div>Alert 2</div>
-                  <div>Alert 3</div>
-                  <div>Alert 4</div>
+                <div className='text-4xl font-semibold tracking-wide font-cabin text-dark mb-5'>Alerts</div>
+                <div className='grid grid-cols-1 divide-y divide-gray-200'>
+                  <AlertCard />
+                  <AlertCard />
+                  <AlertCard />
                 </div>
             </div>
             <div className='col-start-6 col-span-6 bg-white p-8 rounded-3xl my-5'>
